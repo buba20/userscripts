7ad428906ae5aa68ea40e9165cd9caf49872b763
Bartosz <buba20@gmail.com>
2013-12-27 14:24:37 +0100

    Create otomoto.js

---
+++ otomoto.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name       otomoto
+// @namespace  http://use.i.E.your.homepage/
+// @version    0.1
+// @description  enter something useful
+// @match      https://www.google.pl/webhp?sourceid=chrome-instant&espv=210&ie=UTF-8
+// @copyright  2012+, You
+// ==/UserScript==
+
