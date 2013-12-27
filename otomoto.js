92c625f90a069c18ecaef7037d8c4b71eec6562b
Bartosz <buba20@gmail.com>
2013-12-27 14:28:24 +0100

    Update otomoto.js

--- otomoto.js
+++ otomoto.js
@@ -1,9 +1,15 @@
 // ==UserScript==
-// @name       otomoto
+// @name       Otomoto.pl - remove top Ad banner
 // @namespace  http://use.i.E.your.homepage/
 // @version    0.1
-// @description  enter something useful
-// @match      https://www.google.pl/webhp?sourceid=chrome-instant&espv=210&ie=UTF-8
+// @icon       http://static-img.otomoto.pl/main/cobrand/1_0/img/favicon.ico
+// @icon64     http://static-img.otomoto.pl/main/cobrand/1_0/img/favicon.ico
+// @match      http://otomoto.pl/*
 // @copyright  2012+, You
 // ==/UserScript==
+var parent = document.getElementById('topZoneBanner').parentNode;
+parent.removeChild(document.getElementById('topZoneBanner'));
+
+var parentU = document.getElementsByClassName('usabilla_live_button_container').parentNode;
+parentU.removeChild(document.getElementsByClassName('usabilla_live_button_container'));
 
