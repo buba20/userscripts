ed40db7658c4473b46b1dce20a6d17dd0c816bdc
buba20 <buba20@gmail.com>
2013-12-28 12:08:05 +0100

    check updating.

--- otomoto.js
+++ otomoto.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Otomoto.pl - remove top Ad banner
 // @namespace  http://use.i.E.your.homepage/
-// @version    0.1
+// @version    0.2
 // @icon       http://static-img.otomoto.pl/main/cobrand/1_0/img/favicon.ico
 // @icon64     http://static-img.otomoto.pl/main/cobrand/1_0/img/favicon.ico
 // @match      http://otomoto.pl/*
--- otomoto.meta.js
+++ otomoto.meta.js
@@ -1,1 +1,11 @@
-﻿
+﻿// ==UserScript==
+// @name       Otomoto.pl - remove top Ad banner
+// @namespace  http://use.i.E.your.homepage/
+// @version    0.2
+// @icon       http://static-img.otomoto.pl/main/cobrand/1_0/img/favicon.ico
+// @icon64     http://static-img.otomoto.pl/main/cobrand/1_0/img/favicon.ico
+// @match      http://otomoto.pl/*
+// @updateURL       https://raw.github.com/buba20/userscripts/master/otomoto.meta.js
+// @downloadURL     https://raw.github.com/buba20/userscripts/master/otomoto.js     
+// @copyright  2012+, You
+// ==/UserScript==
