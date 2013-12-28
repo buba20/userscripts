400708ad4bc4c2ae19424bc9e995573ce4c39bf2
buba20 <buba20@gmail.com>
2013-12-28 12:13:26 +0100

    refactor

--- otomoto.js
+++ otomoto.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Otomoto.pl - remove top Ad banner
 // @namespace  http://use.i.E.your.homepage/
-// @version    0.2
+// @version    0.3
 // @icon       http://static-img.otomoto.pl/main/cobrand/1_0/img/favicon.ico
 // @icon64     http://static-img.otomoto.pl/main/cobrand/1_0/img/favicon.ico
 // @match      http://otomoto.pl/*
@@ -9,8 +9,21 @@
 // @downloadURL     https://raw.github.com/buba20/userscripts/master/otomoto.js     
 // @copyright  2012+, You
 // ==/UserScript==
-var parent = document.getElementById('topZoneBanner').parentNode;
-parent.removeChild(document.getElementById('topZoneBanner'));
+(function () {
 
-var parentU = document.getElementsByClassName('usabilla_live_button_container').parentNode;
-parentU.removeChild(document.getElementsByClassName('usabilla_live_button_container'));
+    function start() {
+        var parent = document.getElementById('topZoneBanner').parentNode;
+        parent.removeChild(document.getElementById('topZoneBanner'));
+
+        var parentU = document.getElementsByClassName('usabilla_live_button_container').parentNode;
+        parentU.removeChild(document.getElementsByClassName('usabilla_live_button_container'));
+
+    }
+
+    function removeElement(el) {
+        var parent = el.parentNode;
+        parent.removeChild(el);
+    }
+
+    return start();
+})();
